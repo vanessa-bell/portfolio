85c8de2653d84573ab05f60d95c64ba4223b367c
Vanessa Bell <vanessa.bell14@gmail.com>
2017-06-26 16:08:31 -0700

    start

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+  // "grunt" alone creates a new, completed images directory
+  // "grunt clean" removes the images directory
+  // "grunt responsive_images" re-processes images without removing the old ones
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 1600,
+            suffix: '_large_2x',
+            quality: 75
+          },
+          {
+            width: 800,
+            suffix: '_medium_1x',
+            quality: 50
+          },
+          {
+            width: 300,
+            suffix: '_small_1x',
+            quality: 30
+          },
+          ]
+        },
+
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: 'images_src/fixed/*.{gif,jpg,png}',
+          dest: 'images/'
+        }]
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
